710245fe682230b5f8174cb5d5402de93ae1fe7b
gasparcs <gasparcastro.quindamba12@gmail.com>
2026-04-06 19:58:43 +0000

    Update Program.cs

--- SAC/Gest-o-Consultas/src/Backend/Kigramed/Program.cs
+++ SAC/Gest-o-Consultas/src/Backend/Kigramed/Program.cs
@@ -1,4 +1,14 @@
 using Kigramed.K02.Infra.Data;
+using Kigramed.K02.Infra.Reporitory.Auth;
+using Kigramed.K02.Infra.Reporitory.Cliente;
+using Kigramed.K02.Infra.Reporitory.Consulta;
+using Kigramed.K02.Infra.Reporitory.Especialidade;
+using Kigramed.K02.Infra.Reporitory.Ferfil;
+using Kigramed.K02.Infra.Reporitory.Funcionario;
+using Kigramed.K02.Infra.Reporitory.Paciente;
+using Kigramed.K02.Infra.Reporitory.Pagamento;
+using Kigramed.K02.Infra.Reporitory.Servico;
+using Kigramed.K04.Domain.D01.Perfil;
 using Kigramed.K04.Domain.D02.Funcionario;
 using Kigramed.K04.Domain.D05.Auth;
 using Kigramed.K04.Domain.D06.Especialidade;
@@ -21,57 +31,56 @@ string conexao= builder.Configuration.GetConnectionString("ConexaoLocal")!;
 builder.Services.AddDbContext<KigramedDbContext>(options => options.UseNpgsql(conexao));
 
 //Contratos da AuthModel
-builder.Services.AddScoped<IAdicionarRepository<AuthModel>>();
-builder.Services.AddScoped<ILoginRepository>(); 
+builder.Services.AddScoped<IAdicionarRepository<AuthModel>, AdicionarAuthRepository>();
+builder.Services.AddScoped<ILoginRepository, LoginRepositoy>(); 
 //Contratos do Cliente 
-builder.Services.AddScoped<IAdicionarRepository<ClienteModel>>();
-builder.Services.AddScoped<IActualizarRepository<ClienteModel>>();
-builder.Services.AddScoped<IListagemRepository<ClienteModel>>();
-builder.Services.AddScoped<IPegarpeloNifReporitory<ClienteModel>>();
-builder.Services.AddScoped<IPegarpeloTextoRepository<ClienteModel>>();
-builder.Services.AddScoped<IRemoverRepository<ClienteModel>>();
+builder.Services.AddScoped<IAdicionarRepository<ClienteModel>, AdicionarClienteRepository>();
+builder.Services.AddScoped<IActualizarRepository<ClienteModel>, AtualizarClienteRepository>();
+builder.Services.AddScoped<IListagemRepository<ClienteModel>, ListarClientesRepository>();
+builder.Services.AddScoped<IPegarpeloNifReporitory<ClienteModel>, PegarClientepeloNifRepository>();
+builder.Services.AddScoped<IPegarpeloTextoRepository<ClienteModel>,PegarClientepeloTextoRepository >();
+builder.Services.AddScoped<IRemoverRepository<ClienteModel>, RemoverClienteRepository>();
 //Contratos da Consulta
-builder.Services.AddScoped<IAdicionarRepository<ConsultaModel>>();
-builder.Services.AddScoped<IActualizarRepository<ConsultaModel>>();
-builder.Services.AddScoped<IListagemRepository<ConsultaModel>>();
-builder.Services.AddScoped<IPegarpeloId<ConsultaModel>>();
-builder.Services.AddScoped<IRemoverRepository<ConsultaModel>>();
+builder.Services.AddScoped<IAdicionarRepository<ConsultaModel>, AdicionarConsultaRepository>();
+builder.Services.AddScoped<IActualizarRepository<ConsultaModel>, AtualizarConsultaRepository>();
+builder.Services.AddScoped<IListagemRepository<ConsultaModel>, ListarConsultaRepository>();
+builder.Services.AddScoped<IPegarpeloId<ConsultaModel>, PegarIdConsultaRepository>();
+builder.Services.AddScoped<IRemoverRepository<ConsultaModel>, RemoverConsultaRepository>();
 //Contratos da Especialidade
-builder.Services.AddScoped<IAdicionarRepository<EspecialidadeModel>>();
-builder.Services.AddScoped<IActualizarRepository<EspecialidadeModel>>();
-builder.Services.AddScoped<IListagemRepository<EspecialidadeModel>>();
-builder.Services.AddScoped<IPegarpeloId<EspecialidadeModel>>();
-builder.Services.AddScoped<IPegarpeloTextoRepository<EspecialidadeModel>>();
-builder.Services.AddScoped<IRemoverRepository<EspecialidadeModel>>();
+builder.Services.AddScoped<IAdicionarRepository<EspecialidadeModel>, AddEspecialidadeRepository>();
+builder.Services.AddScoped<IActualizarRepository<EspecialidadeModel>, AtualizarEspecialidadeRepository>();
+builder.Services.AddScoped<IListagemRepository<EspecialidadeModel>, ListarEspecialidadeRepository>();
+builder.Services.AddScoped<IPegarpeloId<EspecialidadeModel>, PegaridEspecialidadeRepository>();
+builder.Services.AddScoped<IPegarpeloTextoRepository<EspecialidadeModel>, PegartextoEspecialidadeRepository>();
+builder.Services.AddScoped<IRemoverRepository<EspecialidadeModel>, RemoverEspecialidadeRepository>();
 //Contratos da Perfil
-builder.Services.AddScoped<IListagemRepository<EspecialidadeModel>>();
+builder.Services.AddScoped<IListagemRepository<PerfilModel>, ListarPerfilRepository>();
 //Contratos do Funcionário
-builder.Services.AddScoped<IAdicionarRepository<FuncionarioModel>>();
-builder.Services.AddScoped<IActualizarRepository<FuncionarioModel>>();
-builder.Services.AddScoped<IListagemRepository<FuncionarioModel>>();
-builder.Services.AddScoped<IPegarpeloNifReporitory<FuncionarioModel>>();
-builder.Services.AddScoped<IPegarpeloNifReporitory<FuncionarioModel>>();
-builder.Services.AddScoped<IRemoverRepository<FuncionarioModel>>();
+builder.Services.AddScoped<IAdicionarRepository<FuncionarioModel>, AddFuncionarioRepository>();
+builder.Services.AddScoped<IActualizarRepository<FuncionarioModel>, AtualizarFuncionarioRepository>();
+builder.Services.AddScoped<IListagemRepository<FuncionarioModel>, ListarFuncionarioRepository>();
+builder.Services.AddScoped<IPegarpeloNifReporitory<FuncionarioModel>, PegarnifFuncionarioRepository>();
+builder.Services.AddScoped<IPegarpeloTextoRepository<FuncionarioModel>, PegartextoFuncionarioRepository>();
+builder.Services.AddScoped<IRemoverRepository<FuncionarioModel>, RemoverFuncionarioRepository>();
 //Contrato do Paciente
-builder.Services.AddScoped<IAdicionarRepository<PacienteModel>>();
-builder.Services.AddScoped<IActualizarRepository<PacienteModel>>();
-builder.Services.AddScoped<IListagemRepository<PacienteModel>>();
-builder.Services.AddScoped<IPegarpeloId<PacienteModel>>();
-builder.Services.AddScoped<IPegarpeloNifReporitory<PacienteModel>>();
-builder.Services.AddScoped<IPegarpeloTextoRepository<PacienteModel>>();
-builder.Services.AddScoped<IRemoverRepository<PacienteModel>>();
+builder.Services.AddScoped<IAdicionarRepository<PacienteModel>, AdicionarPacienteRepository>();
+builder.Services.AddScoped<IActualizarRepository<PacienteModel>, AtualizarPacienteRepository>();
+builder.Services.AddScoped<IListagemRepository<PacienteModel>, ListarPacientesRepository>();
+builder.Services.AddScoped<IPegarpeloId<PacienteModel>, PegarPacientepeloIDRepository>();
+builder.Services.AddScoped<IPegarpeloTextoRepository<PacienteModel>, PegarPacientepeloTextoRepository>();
+builder.Services.AddScoped<IRemoverRepository<PacienteModel>, RemoverPacienteRepository>();
 //Contratos para o Pagamento
-builder.Services.AddScoped<IAdicionarRepository<PagamentoModel>>();
-builder.Services.AddScoped<IListagemRepository<PagamentoModel>>();
-builder.Services.AddScoped<IPegarpeloId<PagamentoModel>>();
-builder.Services.AddScoped<IRemoverRepository<PagamentoModel>>();
+builder.Services.AddScoped<IAdicionarRepository<PagamentoModel>, AdicionarPagamentoRepository>();
+builder.Services.AddScoped<IListagemRepository<PagamentoModel>, ListarPagamentoRepository>();
+builder.Services.AddScoped<IPegarpeloId<PagamentoModel>, PegarIdPagamentoRepository>();
+builder.Services.AddScoped<IRemoverRepository<PagamentoModel>, RemoverPagamentoRepository>();
 //Contratos para o Serviço
-builder.Services.AddScoped<IAdicionarRepository<ServicoModel>>();
-builder.Services.AddScoped<IActualizarRepository<ServicoModel>>();
-builder.Services.AddScoped<IListagemRepository<ServicoModel>>();
-builder.Services.AddScoped<IPegarpeloId<ServicoModel>>();
-builder.Services.AddScoped<IPegarpeloTextoRepository<ServicoModel>>();
-builder.Services.AddScoped<IRemoverRepository<ServicoModel>>();
+builder.Services.AddScoped<IAdicionarRepository<ServicoModel>, AdicionarServicoRepository>();
+builder.Services.AddScoped<IActualizarRepository<ServicoModel>, AtualizarServicoRepository>();
+builder.Services.AddScoped<IListagemRepository<ServicoModel>,ListarServicoRepository>();
+builder.Services.AddScoped<IPegarpeloId<ServicoModel>, PegarIdServicoRepository>();
+builder.Services.AddScoped<IPegarpeloTextoRepository<ServicoModel>, PegarTextoServicoRepository>();
+builder.Services.AddScoped<IRemoverRepository<ServicoModel>, RemoverServicoRepository>();
 
 var app = builder.Build();
 
